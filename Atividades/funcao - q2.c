22f0f9e3803782282328e7a387b29d9e6da9ee84
Vinícius Nunes <99910009+vluquini@users.noreply.github.com>
2022-02-19 01:24:24 +0000

    Quest2 de funções.

---
+++ Atividades/funcao - q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+      int Subt(int valor1, int valor2, int valor3);
+      int a, b, c, result;
+    
+      printf("Valor 1: ");
+      scanf("%d", &a);
+      printf("Valor 2: ");
+      scanf("%d", &b);
+      printf("Valor 1: ");
+      scanf("%d", &c);
+      result = Subt(a, b, c);
+      printf("O valor da subtracao eh %d", result);
+    
+      return 0;
+}
+int Subt(int valor1, int valor2, int valor3){
+      int result = valor1 - valor2 - valor3;
+      
+      return result;
+}
